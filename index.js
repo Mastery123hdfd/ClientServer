c96cdf7a851718649f5aa804fd99065e2a22fdf4
Mastery123hdfd <109929808+Mastery123hdfd@users.noreply.github.com>
2026-02-04 16:51:22 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 const WebSocket = require("ws");
 
 const port = process.env.PORT || 10000;
@@ -10,12 +12,26 @@ const clients = new Map();
 
 // Proper history buffer
 const history = [];
+const loginfo = {};
+
+loginfo["mhwenAdminLoginMJC"] = "1142";
+loginfo["testUser1"] ="101";
+loginfo["modOliverLimb20213"] = "30412";
+
+const testPass = "101";
+const adminPass = "1142";
+const modAdminPassArray = ["30412"];
 
 server.on("connection", socket => {
     console.log("Client connected");
 
     let monikerSet = false;
     let firstmessage = true;
+    let loginmsg = false;
+    let passmsg = false;
+    let loginstring = "";
+    let command = false;
+    let passwordstring="";
     socket.send("Please input your moniker");
 
     socket.on("message", msg => {
@@ -23,7 +39,11 @@ server.on("connection", socket => {
 
         // First message = moniker
         if (!monikerSet) {
-            clients.set(socket, msg);
+            clients.set(socket, {
+                moniker: msg,
+                admin: false,
+                mod: false
+            });
             monikerSet = true;
 
             socket.send(`Welcome, ${msg}!`);
@@ -40,9 +60,83 @@ server.on("connection", socket => {
             socket.send("Please input your new username");   
             return;
         }
+        if(passmsg){
+            passwordstring = msg;
+            const user = clients.get(socket);
 
-        const moniker = clients.get(socket) || "UNKNOWN";
-        const taggedMessage = `${moniker}: ${msg}`;
+
+            if (loginstring in loginfo && loginfo[loginstring] == passwordstring) {
+                if(passwordstring == testPass){
+                    socket.send("Test successful, client not marked");
+                }
+                else if(modAdminPassArray.includes(passwordstring)){
+                    socket.send("Account Upgraded to Moderator Status. NOTE: Changing monikers will revoke permissions.");
+                    user.mod = true;
+                    //make mod
+                }
+                else if(passwordstring == adminPass){
+                    socket.send("Welcome, admin. Socket elevated to Admin levels. NOTE: Changing monikers will revoke permissions.");
+                    user.mod = true;
+                    user.admin = true;
+                    //make mod AND admin
+                }
+                else{
+                    socket.send("Incorrect Credentials.");
+                }
+                passmsg = false;
+                return;
+            } else{
+                passmsg = false;
+                socket.send("Incorrect Password");
+                return;
+            }
+        }
+        if(loginmsg){
+            loginstring = msg;
+            if(loginstring in loginfo){
+                passmsg = true;
+                loginmsg = false;
+                return;
+            }else{
+                socket.send("Invalid Login Info");
+                return;
+            }
+        }
+        if(msg == "/login"){
+            socket.send("Please input your login Username");
+            loginmsg = true;
+            return;
+        }
+        if(msg == "/cmd"){
+            const usersocket = clients.get(socket);
+            if(usersocket.mod || usersocket.admin){
+                command = true;
+                return;
+            }
+            else{
+                socket.send("you do not have permission to use this command");
+                return;
+            }
+        }
+        const user = clients.get(socket);
+        const moniker = user.moniker;
+        let taggedMessage = `${moniker}: ${msg}`;
+        if(command){
+            const usersocket = clients.get(socket);
+            if(msg == "/strikemsg"){
+                history.pop();
+                return;
+            }
+            if(msg == "/clearhist" && usersocket.admin){
+                history.length = 0;
+                taggedMessage = "Admin has cleared the chat";
+            }
+            if(msg =="/cmd"){
+                command = false;
+            }
+        }
+
+        
 
         console.log("Broadcast:", taggedMessage);
 
@@ -61,7 +155,8 @@ server.on("connection", socket => {
     });
 
     socket.on("close", () => {
-        const moniker = clients.get(socket);
+         const user = clients.get(socket);
+        const moniker = user ? user.moniker : "Anonymous";
         console.log(`Client disconnected: ${moniker}`);
         clients.delete(socket);
     });

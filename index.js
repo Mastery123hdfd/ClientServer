7048fd5c78bc47efc56eb664128354df23766a99
Mastery123hdfd <109929808+Mastery123hdfd@users.noreply.github.com>
2026-02-10 10:01:06 -0600

    MinorSyntaxFix1
    Fix syntax error in room name validation check.

--- index.js
+++ index.js
@@ -190,7 +190,6 @@ server.on("connection", socket => {
             }
             socket.send("Note; Storage is limited. Please try not to open any Private Rooms if you don't have to. Refer to /help for a list of commands.");
             return;
-        }
         if(user.newName){
           if(!validateRoomName(msg){
             socket.send("Invalid Moniker");

c2d5f90e43527e63aeadbbd55719dc7f083d8df3
Mastery123hdfd <109929808+Mastery123hdfd@users.noreply.github.com>
2026-02-14 01:58:19 -0600

    Update index.js

--- index.js
+++ index.js
@@ -356,6 +356,7 @@ server.on("connection", socket => {
               }
             }
           }
+          return;
         }
 
         // ===================== SESSION RESTORE =====================
